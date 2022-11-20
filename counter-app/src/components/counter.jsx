6a3c69a06400b92d6011cd73e1eb87823d1729e6
zalogarciam <gegarciam95@gmail.com>
2022-11-19 19:34:03 -0500

    Refactor method for badge

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -1,8 +1,8 @@
-import React, { Component, Fragment } from "react";
+import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0,
+    count: 1,
   };
 
   styles = {
@@ -13,13 +13,18 @@ class Counter extends Component {
   render() {
     return (
       <div>
-        <span style={this.styles} className="badge bg-primary m-2">
-          {" "}
-          {this.formatCount()}{" "}
+        <span style={this.styles} className={this.getBadgeClasses()}>
+          {this.formatCount()}
         </span>
         <button className="btn btn-secondary btn-sm">Increment</button>
       </div>
     );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 bg-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
   }
 
   formatCount() {
