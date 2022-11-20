41b88a4a35f57107250bf90512d0cf59f5b0deb4
zalogarciam <gegarciam95@gmail.com>
2022-11-19 20:07:29 -0500

    Map items and key them

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -3,6 +3,7 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 1,
+    tags: ["tag1", "tag2", "tag3"],
   };
 
   styles = {
@@ -17,6 +18,11 @@ class Counter extends Component {
           {this.formatCount()}
         </span>
         <button className="btn btn-secondary btn-sm">Increment</button>
+        <ul>
+          {this.state.tags.map((tag) => (
+            <li key={tag}> {tag} </li>
+          ))}
+        </ul>
       </div>
     );
   }
