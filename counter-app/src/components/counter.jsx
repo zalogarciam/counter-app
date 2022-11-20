24abf9a331d7f82958a7121c23e10a5f0bc29965
zalogarciam <gegarciam95@gmail.com>
2022-11-19 20:20:22 -0500

    Add conditional if

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -11,6 +11,17 @@ class Counter extends Component {
     fontWeight: "bold",
   };
 
+  renderTags() {
+    if (this.state.tags.length === 0) return <p>There are no tags!</p>;
+    return (
+      <ul>
+        {this.state.tags.map((tag) => (
+          <li key={tag}> {tag} </li>
+        ))}
+      </ul>
+    );
+  }
+
   render() {
     return (
       <div>
@@ -18,11 +29,8 @@ class Counter extends Component {
           {this.formatCount()}
         </span>
         <button className="btn btn-secondary btn-sm">Increment</button>
-        <ul>
-          {this.state.tags.map((tag) => (
-            <li key={tag}> {tag} </li>
-          ))}
-        </ul>
+        {this.state.tags.length === 0 && "Please create a new tag!"}
+        {this.renderTags()}
       </div>
     );
   }
