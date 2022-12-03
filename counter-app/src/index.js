a57bd198272657ad23fc2438ca1297ebad05bb0b
zalogarciam <gegarciam95@gmail.com>
2022-12-03 13:05:16 -0500

    Adding navbar

--- counter-app/src/index.js
+++ counter-app/src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css'
-import Counters from './components/counters';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Counters />
+    <App />
   </React.StrictMode>
 );
 
