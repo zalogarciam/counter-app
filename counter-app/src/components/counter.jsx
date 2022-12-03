ff73cc503c4dd70b83b688b440c5faa1a79e5332
zalogarciam <gegarciam95@gmail.com>
2022-12-02 19:03:08 -0500

    Update counter with handleDelete

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    value: this.props.value,
+    value: this.props.counter.value,
     tags: ["tag1", "tag2", "tag3"],
   };
 
@@ -36,7 +36,6 @@ class Counter extends Component {
     console.log(this.props);
     return (
       <div>
-        {this.props.children}
         <span style={this.styles} className={this.getBadgeClasses()}>
           {this.formatCount()}
         </span>
@@ -46,6 +45,7 @@ class Counter extends Component {
         >
           Increment
         </button>
+        <button onClick={() => this.props.onDelete(this.props.counter.id)} className="btn btn-danger btm-sm m-2">Delete</button>
 
         {this.state.tags.length === 0 && "Please create a new tag!"}
         {this.renderTags()}
--- counter-app/src/components/counters.jsx
+++ counter-app/src/components/counters.jsx
@@ -10,11 +10,21 @@ class Counters extends Component {
       { id: 4, value: 0 },
     ],
   };
+
+  handleDelete = (counterId) => {
+    const counters = this.state.counters.filter((c) => c.id !== counterId);
+    this.setState({ counters });
+  };
+
   render() {
     return (
       <div>
         {this.state.counters.map((counter) => (
-          <Counter key={counter.id} value={counter.value}>
+          <Counter
+            key={counter.id}
+            onDelete={this.handleDelete}
+            counter = {counter}
+          >
             <h4> Counter #{counter.id} </h4>
           </Counter>
         ))}
