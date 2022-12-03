bfcb126ee14a1841cd7e7e826256562a9bff5330
zalogarciam <gegarciam95@gmail.com>
2022-12-03 13:17:04 -0500

    Change app function to class

--- counter-app/src/App.js
+++ counter-app/src/App.js
@@ -1,18 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
-import NavBar from './components/navbar';
-import Counters from './components/counters';
-import { Fragment } from 'react';
+import logo from "./logo.svg";
+import "./App.css";
+import NavBar from "./components/navbar";
+import Counters from "./components/counters";
+import { Fragment } from "react";
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <Fragment>
-      <NavBar></NavBar>
-      <main className='container'>
-        <Counters></Counters>
-      </main>
-    </Fragment>
-  );
+class App extends Component {
+  state = {
+    counters: [
+      { id: 1, value: 0 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 },
+    ],
+  };
+
+  handleDelete = (counterId) => {
+    const counters = this.state.counters.filter((c) => c.id !== counterId);
+    this.setState({ counters });
+  };
+
+  handleReset = () => {
+    const counters = this.state.counters.map((c) => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters });
+  };
+
+  handleIncrement = (counter) => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value++;
+    this.setState({ counters });
+  };
+
+  render() {
+    return (
+      <Fragment>
+        <NavBar></NavBar>
+        <main className="container">
+          <Counters
+            onReset={this.handleReset}
+            onIncrement={this.handleIncrement}
+            onDelete={this.handleDelete}
+            counters={this.state.counters}
+          ></Counters>
+        </main>
+      </Fragment>
+    );
+  }
 }
 
 export default App;
